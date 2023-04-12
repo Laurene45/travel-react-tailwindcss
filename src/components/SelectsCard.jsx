4aae41d884d5bd7dd8bff82ce4077486b5efd552
Laurene COURDE <laurene.courde@gmail.com>
2023-04-12 17:16:42 +0200

    ajout fichiers app react /tailwind travel

---
+++ src/components/SelectsCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SelectsCard = (props) => {
+  return (
+    <div className="relative">
+      <img className="w-full h-full object-cover" src={props.bg} alt="/" />
+      <div className="bg-gray-900/30 absolute top-0 left-0 w-full h-full">
+        <p className="left-4 bottom-4 text-2xl font-bold text-white absolute">
+          {props.text}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+SelectsCard.propTypes = {
+  text: PropTypes.string,
+  bg : PropTypes.string,
+};
+
+
+export default SelectsCard;
